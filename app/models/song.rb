06c24c13a2c60208f45e1e80f6abace6b02a79ea
Laura Felce <crepu83@yahoo.it>
2019-06-11 08:56:21 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,33 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+
+  def artist_name=(name)
+
+  end
+
+  def artist_name
+
+  end
+
+  def genre_name=(name)
+
+  end
+
+
+  def genre_name
+
+  end
+
+  def note_contents=(content)
+
+  end
+
+  def note_contents
+
+  end
+
+
 end
