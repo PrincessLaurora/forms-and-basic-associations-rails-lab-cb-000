562885b5c48846521b49cd331be39073455dfdee
Laura Felce <crepu83@yahoo.it>
2019-06-11 09:21:44 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -21,7 +21,7 @@ class Song < ActiveRecord::Base
     self.genre ? self.genre.name : nil
   end
 
-  def note_contents=(content)
+  def note_contents=(contents)
     contents.each do |content|
       note = Note.find(content)
       self.notes << content
